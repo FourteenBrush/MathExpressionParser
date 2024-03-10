5c637fa9be8b8425dd77efce6c740e8693c5d87b
FourteenBrush <naessensarthur2@protonmail.com>
2024-03-10 16:55:09 +0100

    Rename cli module to repl

---
+++ repl/src/main/java/me/fourteendoggo/mathexpressionparser/Main.java
@@ -0,0 +1,35 @@
+package me.fourteendoggo.mathexpressionparser;
+
+import me.fourteendoggo.mathexpressionparser.symbol.ExecutionEnv;
+
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class Main {
+    private static boolean running = true;
+
+    public static void main(String[] args) {
+        ExecutionEnv env = ExecutionEnv.createDefault();
+        env.insertFunction("exit", () -> {
+            running = false;
+            return 0;
+        });
+
+        Scanner in = new Scanner(System.in);
+
+        while (running) {
+            System.out.print("Enter an expression or type exit() to exit: ");
+            try {
+                String input = in.nextLine();
+                long now = System.nanoTime();
+                double result = ExpressionParser.parse(input, env);
+                double elapsedMicros = (System.nanoTime() - now) / 1000.0;
+                System.out.printf("[✔] Result: %s (%s µs)%n", result, elapsedMicros);
+            } catch (NoSuchElementException e) { // ctrl-c
+                break;
+            } catch (Throwable t) {
+                System.out.printf("\u001B[31m[✘] Error: %s\u001B[0m\n", t.getMessage());
+            }
+        }
+    }
+}
